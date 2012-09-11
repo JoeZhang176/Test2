7ced0190d034fe790caabc046898db57d4375e49
Joe.Zhang <zhangkai@Macfocus-Michelle.local>
2012-09-11 11:11:05 +0800

    add a root view controller

---
+++ Test2/Test2/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Test2
+//
+//  Created by Joe.Zhang on 9/11/12.
+//  Copyright (c) 2012 EF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ Test2/Test2/RootViewController.m
@@ -0,0 +1,43 @@
+//
+//  RootViewController.m
+//  Test2
+//
+//  Created by Joe.Zhang on 9/11/12.
+//  Copyright (c) 2012 EF. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
